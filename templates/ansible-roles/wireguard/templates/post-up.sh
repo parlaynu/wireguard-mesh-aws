9338734d8e4a50845d6c56b2b7c8237c7a4bd115
Paul Ryan <paul@reillyryan.net>
2022-06-20 09:59:37 +1000

    drop forwarding after specific forwarding rules in place

--- templates/ansible-roles/wireguard/templates/post-up.sh
+++ templates/ansible-roles/wireguard/templates/post-up.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-iptables -P FORWARD DROP
 iptables -A FORWARD -i eth0 -o eth0 -j ACCEPT
 iptables -A FORWARD -i wg0 -o eth0 -d {{ cidr_block }} -j ACCEPT
 iptables -A FORWARD -o wg0 -i eth0 -s {{ cidr_block }} -j ACCEPT
+iptables -P FORWARD DROP
 
 iptables -t nat -A POSTROUTING -o eth0 ! -d {{ cidr_block }} -j MASQUERADE
 
